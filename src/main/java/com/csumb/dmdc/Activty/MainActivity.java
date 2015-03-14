fb54b964edac5b77ac7088a7fea6493545453c7e
banal_a <axel.banal@epita.fr>
2015-03-13 20:08:55 -0700

    Initial Commit

--- src/main/java/com/csumb/dmdc/Activty/MainActivity.java
+++ src/main/java/com/csumb/dmdc/Activty/MainActivity.java
@@ -1,9 +1,12 @@
-package com.csumb.dmdc;
+package com.csumb.dmdc.Activty;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.csumb.dmdc.R;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
     }
 
 
